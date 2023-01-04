4adb471e312ac9d6c967206af21a8e674c8cf4d8
AlexCioca <alex.cioca18@gmail.com>
2023-01-04 12:40:35 +0200

    Initial commit

---
+++ src/test/java/com/shop/tema3/Tema3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.tema3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tema3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
