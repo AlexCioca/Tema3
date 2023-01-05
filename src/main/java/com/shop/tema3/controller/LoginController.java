42c50c6bc82005ce7afe202645cb8ecdb098b796
AlexCioca <alex.cioca18@gmail.com>
2023-01-05 19:34:58 +0200

    added config

--- src/main/java/com/shop/tema3/controller/LoginController.java
+++ src/main/java/com/shop/tema3/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.shop.tema3.controller;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,15 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/*")
+@RequestMapping("/login")
 @RequiredArgsConstructor
-public class HomeController {
+public class LoginController {
 
 
     @GetMapping()
     public String open(Model model, String error, String logout)
     {
-        return "index";
+        return "login";
     }
 
 }
